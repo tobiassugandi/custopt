743900bbe9bbf98ede20a382f9f710c765902b2c
Tobias Sugandi <suganditobias@gmail.com>
2024-09-04 22:54:07 +0200

    add Nystrom Initialization support for the precond hessian spectral density

--- custopt/precond_hessian.py
+++ custopt/precond_hessian.py
@@ -1,39 +1,57 @@
+'''
+Adapted from the implementation of Rathore et al. (2024) (https://github.com/pratikrathore8/opt_for_pinns)
+'''
+
 import torch
 from .nysopt import _apply_nys_inv
 
 class Precond_hessian():
   """
-  Class for computing spectral density of (L-BFGS) pre-conditioned Hessian. 
+  Class for computing spectral density of ((Nys-)L-BFGS) pre-conditioned Hessian. 
 
-  - model: instance of PINN
-  - pred_func: prediction function
-  - loss_func: loss function
   - optimizer: L-BFGS instance used to optimize the model
-  - data: tuple of spatial and temporal inputs
-  - loss_comp: string indicating which component of the loss to use (will use total loss for any value other than "res", "bc", or "ic")
-  - device: string indicating which CUDA device to use (where both model and data reside)
+  - model: instance of PINN
+  - grad_tuple = torch.autograd.grad(loss, self.model.parameters(), create_graph=True
+  - device: string indicating which device to use (where both model and data reside)
   """
   def __init__(self, optimizer, model, grad_tuple, device='cuda'):
-    self.model = model.eval() 
-    self.device = device
-    self.optimizer = optimizer
-    self.state = optimizer.state["global_state"]
+    self.model      = model.eval() 
+    self.device     = device
+    # self.optimizer  = optimizer
 
-    # pre-compute results needed for matrix-vector-products
-    # compute components of tilde_H_k
-    try:
-      history_size = len(optimizer.state_dict()["state"][0]['old_dirs'])
-      y_hist = optimizer.state_dict()["state"][0]['old_dirs']
-      s_hist = optimizer.state_dict()["state"][0]['old_stps']
-      rho_hist = optimizer.state_dict()["state"][0]['ro']
-      gamma = optimizer.state_dict()["state"][0]['H_diag']
-    except:
-      history_size = len(optimizer.state_dict()["state"]['global_state']['old_dirs'])
-      y_hist    = [y.to(device) for y in optimizer.state_dict()["state"]['global_state']['old_dirs'] ]
-      s_hist    = [s.to(device) for s in optimizer.state_dict()["state"]['global_state']['old_stps'] ]
-      rho_hist  = [rho.to(device) for rho in optimizer.state_dict()["state"]['global_state']['rho'] ]
-      gamma     = optimizer.state_dict()["state"]['global_state']['H_diag'].to(device)
+    self.params = [param for param in self.model.parameters() if param.requires_grad]
+    self.param_length = sum( 2 * p.numel() if torch.is_complex(p) else p.numel() for p in self.params ) # optimizer._numel_cache
+    # grad_tuple = torch.autograd.grad(loss, self.model.parameters(), create_graph=True)
+    self.gradsH = [gradient if gradient is not None else 0.0 for gradient in grad_tuple]
 
+    # Extract L-BFGS variables
+    try: # pytorch's L-BFGS
+      state = optimizer.state_dict()["state"][0]
+      history_size = len(state['old_dirs'])
+      y_hist    = state['old_dirs']
+      s_hist    = state['old_stps']
+      rho_hist  = state['ro']
+      gamma     = state['H_diag']
+    except: # hjmshi L-BFGS
+      state       = optimizer.state["global_state"]
+      history_size = len(state['old_dirs'])
+      y_hist      = [y.to(device) for y in state['old_dirs'] ]
+      s_hist      = [s.to(device) for s in state['old_stps'] ]
+      rho_hist    = [rho.to(device) for rho in state['rho'] ]
+      gamma       = state['H_diag'].to(device)
+      
+      self.H0     = state.get("H0")
+      self.U      = state.get("U")
+      self.S      = state.get("S")
+      self.nys_mu = state.get("nys_mu")
+
+    self.history_size   = history_size
+    self.y_hist         = y_hist
+    self.rho_hist       = rho_hist
+    self.gamma          = gamma
+
+
+    # Compute: The factorization tilde_H_k tilde_H_k^T of the LBFGS inverse hessian
     tilde_v_vecs = [None] * history_size
     tilde_s_vecs = [None] * history_size
 
@@ -48,28 +66,16 @@ class Precond_hessian():
       tilde_v_terms = [rho * y.dot(s_hist[i]) * tilde_v for rho, y, tilde_v in zip(rho_hist[i+1:], y_hist[i+1:], tilde_v_vecs[i+1:])]
       tilde_v_vecs[i] = s_hist[i] - torch.stack(tilde_v_terms, 0).sum(0)
 
-    self.history_size = history_size
-    self.y_hist = y_hist
-    self.rho_hist = rho_hist
-    self.gamma = gamma
     self.tilde_v_vecs = tilde_v_vecs
     self.tilde_s_vecs = tilde_s_vecs
-
-    # grad_tuple = torch.autograd.grad(loss, self.model.parameters(), create_graph=True)
-
-    self.params = [param for param in self.model.parameters() if param.requires_grad]
-    self.gradsH = [gradient if gradient is not None else 0.0 for gradient in grad_tuple]
-    self.param_length = sum(
-                2 * p.numel() if torch.is_complex(p) else p.numel()
-                for p in self.params
-            )# optimizer._numel_cache
 
 
 
 
 
   """
-  Function for computing matrix vector product of matrix (tilde_H_k.T Hessian tilde_H_k) and vector v. 
+  Function for computing matrix vector product of matrix 
+    (P^-1/2 Hessian P^-1/2) = (tilde_H_k.T Hessian tilde_H_k) and vector v. 
 
   INPUT: 
   - v: tensor of size (num of model params + history size of L-BFGS)
@@ -77,26 +83,26 @@ class Precond_hessian():
   - mv: tensor of size (num of model params + history size of L-BFGS)
   """
   def matrix_vector_product(self, v): 
-    # step 1: compute product of tilde_H_k and v
+    # step 1: compute mvp tilde_H_k @ v
     # compute v_prime
     v1 = v[:-self.history_size]
     v2 = v[-self.history_size:]
+    if not self.H0 == "Nys":
+      v1 = self.gamma.sqrt() * v1
+    else:
+      v1 = _apply_nys_inv(v1, self.U, self.S, self.nys_mu, 
+                                    pow=-0.5)
     v_prime = [rho * y.dot(v1) for rho, y in zip(self.rho_hist, self.y_hist)]
     v_prime = v1 - torch.stack([v_i * tilde_v for v_i, tilde_v in zip(v_prime, self.tilde_v_vecs)], 0).sum(0)
-    if not self.state.get("H0") == "Nys":
-      v_prime = self.gamma.sqrt() * v_prime
-    else:
-      v_prime = _apply_nys_inv(v_prime, self.state.get("U"), self.state.get("S"), self.state.get("nys_mu"), 
-                                    pow=-0.5)
     v_prime = v_prime + torch.stack([v_i * tilde_s for v_i, tilde_s in zip(v2, self.tilde_s_vecs)], 0).sum(0)
     
-    # step 2: compute Hv_prime using HVP
+    # step 2: compute Hv_prime using autograd
     # convert tensor to a list of tensors matching model parameters
     v_prime_list = []
     offset = 0
     for p in self.params: 
       numel = p.numel()
-      v_prime_list.append(v_prime[offset:offset + numel].reshape(p.size()))
+      v_prime_list.append( v_prime[offset : offset + numel].reshape( p.size() ) )
       offset += numel
     hv_prime = torch.autograd.grad(self.gradsH, self.params, grad_outputs=v_prime_list, only_inputs=True, retain_graph=True)
     # flatten result
@@ -105,20 +111,17 @@ class Precond_hessian():
       views.append(p.reshape(-1))
     hv_prime = torch.cat(views, 0)
     
-    # step 3: compute product of tilde_H_k^T and Hv_prime
-    v_prime = [tilde_v.dot(hv_prime) for tilde_v in self.tilde_v_vecs]
-    v_prime = torch.stack([v_i * rho * y for rho, y, v_i in zip(self.rho_hist, self.y_hist, v_prime)], 0).sum(0)
-    if not self.state["H0"] == "Nys":
-      v_prime = self.gamma.sqrt() * (hv_prime - v_prime)
-      # v_prime = self.gamma.sqrt() * v_prime
+    # step 3: compute mvp tilde_H_k^T @ Hv_prime
+    v1 = [tilde_v.dot(hv_prime) for tilde_v in self.tilde_v_vecs]
+    v1 = torch.stack([v_i * rho * y for rho, y, v_i in zip(self.rho_hist, self.y_hist, v1)], 0).sum(0)
+    if not self.H0 == "Nys":
+      v1 = self.gamma.sqrt() * (hv_prime - v1)
     else:
-      v_prime = _apply_nys_inv((hv_prime - v_prime), self.state.get("U"), self.state.get("S"), self.state.get("nys_mu"), 
+      v1 = _apply_nys_inv((hv_prime - v1), self.U, self.S, self.nys_mu, 
                                pow=-0.5)
+    v2 = torch.tensor([tilde_s.dot(hv_prime) for tilde_s in self.tilde_s_vecs], device=self.device)
     
-    v_double_prime = torch.tensor([tilde_s.dot(hv_prime) for tilde_s in self.tilde_s_vecs], device=self.device)
-    mv = torch.cat([v_prime, v_double_prime], 0)
-
-    return mv
+    return torch.cat([v1, v2], 0)
 
   """
   Function for performing spectral density computation. 
@@ -141,7 +144,7 @@ class Precond_hessian():
 
       # Lanczos initlization
       v_list = [v]
-      w_list = []
+      # w_list = []
       alpha_list = []
       beta_list = []
 
@@ -154,7 +157,7 @@ class Precond_hessian():
           alpha = w_prime.dot(v)
           alpha_list.append(alpha.cpu().item())
           w = w_prime - alpha * v
-          w_list.append(w)
+          # w_list.append(w)
         else:
           beta = torch.sqrt(w.dot(w))
           beta_list.append(beta.cpu().item())
--- custopt/pyhessian.py
+++ custopt/pyhessian.py
@@ -1,8 +1,8 @@
 '''
-Original paper: PyHessian: Neural Networks Through the Lens of the Hessian (https://arxiv.org/abs/1912.07145)
-Original authors implementation: https://github.com/amirgholami/PyHessian
-
-Code is adapted to work with PINNs. 
+Adapted from the implementation of Rathore et al. (2024) (https://github.com/pratikrathore8/opt_for_pinns), 
+which in turn is adapted from:
+  Original paper: PyHessian: Neural Networks Through the Lens of the Hessian (https://arxiv.org/abs/1912.07145)
+  Original authors implementation: https://github.com/amirgholami/PyHessian
 '''
 
 import torch
@@ -13,7 +13,7 @@ import numpy as np
 """
 Compute the inner product of two lists of tensors xs, ys.
 """
-def group_product(xs, ys):
+def list_dot_product(xs, ys):
   return sum([torch.sum(x * y) for (x, y) in zip(xs, ys)])
 
 """
@@ -28,7 +28,7 @@ def group_add(params, update, alpha=1):
 Normalize a vector (represented as a list of tensors). . 
 """
 def normalization(v):
-  s = group_product(v, v)
+  s = list_dot_product(v, v)
   s = s ** 0.5
   s = s.cpu().item()
   v = [vi / (s + 1e-6) for vi in v]
@@ -39,7 +39,7 @@ Orthonormalize vector w to a list of vectors (v_list).
 """
 def orthonormalization(w, v_list):
   for v in v_list:
-    w = group_add(w, v, alpha=-group_product(w, v))
+    w = group_add(w, v, alpha=-list_dot_product(w, v))
   return normalization(w)
 
 """
@@ -55,12 +55,9 @@ class Hessian():
   """
   Class for computing spectral density of (L-BFGS) pre-conditioned Hessian. 
 
-  - model: instance of PINN
-  - pred_func: prediction function
-  - loss_func: loss function
-  - data: tuple of spatial and temporal inputs
-  - loss_comp: string indicating which component of the loss to use (will use total loss for any value other than "res", "bc", or "ic")
-  - device: string indicating which CUDA device to use (where both model and data reside)
+  - model: instance of NN
+  - grad_tuple: tuple of gradients of the loss w.r.t. the model parameters
+  - device: string indicating which device to use
   """
   def __init__(self, model, grad_tuple, device='cuda'):
     self.model = model.eval()
@@ -69,69 +66,12 @@ class Hessian():
     self.gradsH = [gradient if gradient is not None else 0.0 for gradient in grad_tuple]
 
 
-
-  # def __init__(self, model, data, loss_comp=None, device='cuda'):
-  #   self.model = model.eval()
-  #   # self.pred_func = pred_func
-  #   # self.loss_func = loss_func
-  #   # self.x, self.t = data
-  #   self.device = device
-
-
-  #   def compute_fourier_derivative(u, s=None, domain_length = 2 ):
-  #     if s is None:
-  #         s = u.shape[-1]
-          
-  #     # pad = 0
-  #     # padded = torch.nn.functional.pad(u, (pad,pad), mode='circular')
-
-  #     # Compute Fourier transform
-  #     output_fft = torch.fft.fft(u, dim=-1)
-
-  #     # Create the modes
-  #     k_max = s//2
-  #     k = torch.cat((torch.arange(start=0, end=k_max, step=1, device=u.device),
-  #                     torch.arange(start=-k_max, end=0, step=1, device=u.device)), 0).reshape(1,1,s)
-  #     k =k * 2j * torch.pi / domain_length
-
-  #     # Multiply with modes to compute derivative in the frequency domain
-  #     d_fft = k * output_fft
-  #     d2_fft = k ** 2  * output_fft 
-
-  #     # print(d_fft.shape, d2_fft.shape)    
-  #     du, d2u = torch.fft.irfft(d_fft, n=s, dim=-1), torch.fft.irfft(d2_fft, n=s, dim=-1)
-  #     du, d2u = du, d2u
-  #     # print(du.shape, d2u.shape)
-
-  #     return du, d2u
-    
-  #   l = torch.nn.L1Loss()
-  #   self.model.zero_grad()
-
-  #   input_batch, output_batch, out_data_std = data
-  #   output_pred_batch   = model(input_batch) 
-  #   loss_data           = l(output_pred_batch, output_batch) \
-  #                         + torch.nn.MSELoss()(output_pred_batch * 0, torch.zeros_like(output_pred_batch))  # second term ensures all model parameters are in the graph
-
-  #   out_ori             = output_pred_batch * out_data_std
-  #   du, d2u             = compute_fourier_derivative(out_ori)
-  #   declude             = 1
-  #   loss_pde            = l(-d2u[:, 0, declude:-declude], input_batch[:, 1, declude:-declude])
-
-  #   loss                = loss_data + loss_pde
-
-  #   grad_tuple = torch.autograd.grad(loss, self.model.parameters(), create_graph=True)
-
-        
-  #   self.params = [param for param in self.model.parameters() if param.requires_grad]
-  #   self.gradsH = [gradient if gradient is not None else 0.0 for gradient in grad_tuple]
-
   """
-  Function for performing spectral density computation. 
+  Function for performing spectral density computation using Stochastic Lanczos Quadrature. 
 
   INPUT: 
-  - num_iter: number of iterations for Lanczos
-  - num_run: number of runs
+  - num_iter: number of Lanczos iterations
+  - num_run: number of Lanczos quadrature repetitions (number of samples for the expectation)
   OUTPUT: 
   - eigen_list_full: list eigenvalues for each run
   - weight_list_full: list of corresponding densities for each run
@@ -141,13 +81,13 @@ class Hessian():
     weight_list_full = []
 
     for k in range(num_run):
-      # generate Rademacher random variables
+      # generate Rademacher random vector
       v = [2 * torch.randint_like(p, high=2, device=self.device) - 1 for p in self.params]
       v = normalization(v)
 
       # Lanczos initlization
       v_list = [v]
-      w_list = []
+      # w_list = []
       alpha_list = []
       beta_list = []
 
@@ -157,12 +97,12 @@ class Hessian():
         w_prime = [torch.zeros(p.size()).to(self.device) for p in self.params]
         if i == 0:
           w_prime = hessian_vector_product(self.gradsH, self.params, v)
-          alpha = group_product(w_prime, v)
+          alpha = list_dot_product(w_prime, v)
           alpha_list.append(alpha.cpu().item())
           w = group_add(w_prime, v, alpha=-alpha)
-          w_list.append(w)
+          # w_list.append(w)
         else:
-          beta = torch.sqrt(group_product(w, w))
+          beta = torch.sqrt(list_dot_product(w, w))
           beta_list.append(beta.cpu().item())
           if beta_list[-1] != 0.:
             v = orthonormalization(w, v_list)
@@ -172,7 +112,7 @@ class Hessian():
             v = orthonormalization(w, v_list)
             v_list.append(v)
           w_prime = hessian_vector_product(self.gradsH, self.params, v)
-          alpha = group_product(w_prime, v)
+          alpha = list_dot_product(w_prime, v)
           alpha_list.append(alpha.cpu().item())
           w_tmp = group_add(w_prime, v, alpha=-alpha)
           w = group_add(w_tmp, v_list[-2], alpha=-beta)
@@ -227,7 +167,7 @@ class Hessian():
             self.model.zero_grad()
 
             Hv = hessian_vector_product(self.gradsH, self.params, v)
-            tmp_eigenvalue = group_product(Hv, v).cpu().item()
+            tmp_eigenvalue = list_dot_product(Hv, v).cpu().item()
 
             v = normalization(Hv)
 
